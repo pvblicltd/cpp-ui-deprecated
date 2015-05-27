d21e47b122412f039ec4a60ea10b142e4d95d3d6
James Gray <jbg@pvblic.co>
2015-05-27 10:25:18 +0100

    feat(gh-pages): initial gh-pages commit

---
+++ app.js
@@ -0,0 +1,57 @@
+angular.module('app', [
+  'ngRoute',
+  'angularSmoothscroll',
+  'ui.cpp',
+  'ui.bootstrap',
+  'ui.utils',
+  'hljs'
+]);
+angular.module('app')
+  .config(function($routeProvider,$locationProvider){
+    $locationProvider.hashPrefix('!');
+    $routeProvider
+      .when('/modules', {
+        templateUrl:'demo/modules.html'
+      })
+      .when('/css', {
+        templateUrl:'demo/css.html'
+      })
+      .when('/layouts', {
+        templateUrl:'demo/css.html'
+      })
+      .otherwise({
+        redirectTo:'/',
+        templateUrl:'demo/getting-started.html'
+      });
+  })
+  .config(function (hljsServiceProvider) {
+    hljsServiceProvider.setOptions({
+      // replace tab with 4 spaces
+      tabReplace: '  '
+    });
+  });
+angular.module('app').controller('demoModuleDemoCtrl', function ($scope) {
+
+  // Start your demo code here
+
+});
+angular.module('app').controller('globalNavDemoCtrl', function ($scope) {
+
+  // Start your demo code here
+
+});
+angular.module('app').controller('globalSearchDemoCtrl', function ($scope) {
+
+  // Start your demo code here
+
+});
+angular.module('app').controller('jamesTestDemoCtrl', function ($scope) {
+
+  // Start your demo code here
+
+});
+angular.module('app').controller('showingModuleDemoCtrl', function ($scope) {
+
+  // Start your demo code here
+
+});
