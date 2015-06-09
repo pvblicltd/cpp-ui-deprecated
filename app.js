59a1d45163ade7b3de2e08abd3b10709f5303458
James Gray <jbg@pvblic.co>
2015-06-09 10:11:29 +0100

    fix(merge): merging in new pages

---
+++ app.js
@@ -0,0 +1,57 @@
+angular.module('app', [
+  'ngRoute',
+  'angularSmoothscroll',
+  'ui.cpp',
+  'ui.bootstrap',
+  'ui.utils',
+  'hljs'
+]);
+angular.module('app')
+  .config(function($routeProvider,$locationProvider){
+    $locationProvider.hashPrefix('!');
+    $routeProvider
+      .when('/modules', {
+        templateUrl:'modules.html'
+      })
+      .when('/css', {
+        templateUrl:'css.html'
+      })
+      .when('/layouts', {
+        templateUrl:'layouts.html'
+      })
+      .otherwise({
+        redirectTo:'/',
+        templateUrl:'getting-started.html'
+      });
+  })
+  .config(function (hljsServiceProvider) {
+    hljsServiceProvider.setOptions({
+      // replace tab with 4 spaces
+      tabReplace: '  '
+    });
+  });
+angular.module('app').controller('demoModuleDemoCtrl', function ($scope) {
+
+  // Start your demo code here
+
+});
+angular.module('app').controller('globalNavDemoCtrl', function ($scope) {
+
+  // Start your demo code here
+
+});
+angular.module('app').controller('globalSearchDemoCtrl', function ($scope) {
+
+  // Start your demo code here
+
+});
+angular.module('app').controller('jamesTestDemoCtrl', function ($scope) {
+
+  // Start your demo code here
+
+});
+angular.module('app').controller('showingModuleDemoCtrl', function ($scope) {
+
+  // Start your demo code here
+
+});
