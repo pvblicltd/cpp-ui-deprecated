5cfaa3e234f31acc6e64fef11632fa283ac2e2c7
James Gray <jbg@pvblic.co>
2015-07-02 15:47:19 +0100

    fix(padding): updated padding

--- dist/ui.cpp-tpls.js
+++ dist/ui.cpp-tpls.js
@@ -1,7 +1,7 @@
 /*
  * cpp-ui
  * Author: James A B Gray
- * Version: 0.0.4 - 2015-06-25
+ * Version: 0.0.4 - 2015-07-02
  * License: 
  */
 angular.module("ui.cpp", ["ui.cpp.tpls", "ui.cpp.global-nav"]);
